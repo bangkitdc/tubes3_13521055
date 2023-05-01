ce324aaf22037c4d16243ccc357d7f2feb4409e7
auliamey <auliaannandya@gmail.com>
2023-05-02 00:07:06 +0700

    dummy data

--- src/components/Chat/Conversation.tsx
+++ src/components/Chat/Conversation.tsx
@@ -6,10 +6,16 @@ import { useSession } from 'next-auth/react';
 
 interface Message {
   text: string;
-  role: 'user' | 'bot';
+  role: string;
+  room: number;
 }
 
-const Conversation = (): JSX.Element => {
+interface ConversationProps {
+  selectedAlgorithm: string;
+  dummyData: Message[];
+}
+
+const Conversation = ({ selectedAlgorithm, dummyData }: ConversationProps): JSX.Element => {
   const router = useRouter();
   const { id } = router.query;
   const [userMessages, setUserMessages] = useState<Message[]>([]);
@@ -19,7 +25,7 @@ const Conversation = (): JSX.Element => {
   const botMessagesRef = useRef<HTMLDivElement>(null);
   const [lastDisplayedUserMessageIndex, setLastDisplayedUserMessageIndex] = useState<number>(-1);
   const [lastDisplayedBotMessageIndex, setLastDisplayedBotMessageIndex] = useState<number>(-1);
-
+  const [dummyDataAdded, setDummyDataAdded] = useState<boolean>(false);
   const { data: session }: any = useSession();
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -29,7 +35,19 @@ const Conversation = (): JSX.Element => {
   function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     if (currentMessage !== '') {
-      setUserMessages([...userMessages, { text: currentMessage, role: 'user' }]);
+      setUserMessages([...userMessages, { text: currentMessage, role: 'sender' , room: 1}]);
+      // Switch statement to select appropriate response
+      let response = '';
+      switch (selectedAlgorithm) {
+        case 'algorithm1':
+          alert('Response from kmp');
+          break;
+        case 'algorithm2':
+          alert('Response from bm');
+          break;
+        default:
+          alert('haha ga mencet lu');
+      }
     }
     setCurrentMessage('');
     sendMessage();
@@ -46,13 +64,28 @@ const Conversation = (): JSX.Element => {
         "/api/data/qna"
       );
       if (apiRes?.data?.success) {
-        setBotMessages([...botMessages, { text: apiRes.data.messages[0].answer, role: 'bot' }]);
+        setBotMessages([...botMessages, { text: apiRes.data.messages[0].answer, role: 'receiver' , room: 1}]);
       }
     } catch (error) {
       console.error(error);
       alert('An error occurred while sending the message.');
     }
   };
+
+  // ini misah dummy data ke user sm bot
+  useEffect(() => {
+    const userMessages: Message[] = [];
+    const botMessages: Message[] = [];
+    dummyData.forEach((data) => {
+      if (data.role === "sender") {
+        userMessages.push(data);
+      } else {
+        botMessages.push(data);
+      }
+    });
+    setUserMessages(userMessages);
+    setBotMessages(botMessages);
+  }, [dummyData]);
 
   useEffect(() => {
     const userMessagesElement = userMessagesRef.current;
@@ -63,6 +96,7 @@ const Conversation = (): JSX.Element => {
     if (botMessagesElement) {
       botMessagesElement.scrollIntoView()
     }
+    
   }, [userMessages, botMessages]);
 
   useEffect(() => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,14 +21,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <div className="grid grid-cols-5 h-screen w-screen bg-gray-50 dark:bg-gray-900">
-          <div className="col-span-1 py-5 pl-5 pr-2 dark:bg-gray-800">
-            <ChatHistory />
-          </div>
-          <div className="col-span-4 p-5 overscroll-y-contain">
-            <Conversation />
-          </div>
-        </div>
+        <ChatHistory/>
       </div>
     </>
   );
